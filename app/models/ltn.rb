dfcc3dc4d4eff39628a4626cf82aeeb0721596ca
Nikolai B <nikolai-b@users.noreply.github.com>
2025-05-20 20:46:47 +0100

    Reduce scheme name

--- app/models/ltn.rb
+++ app/models/ltn.rb
@@ -2,7 +2,7 @@ class Ltn < ApplicationRecord
   belongs_to :user
   has_many :journeys
   has_many :runs
-  validates :scheme_name, presence: true, length: {minimum: 3, maximum: 45},
+  validates :scheme_name, presence: true, length: {minimum: 3, maximum: 25},
                    uniqueness: {scope: :user}
   validates :default_lat, presence: true
   validates :default_lng, presence: true
